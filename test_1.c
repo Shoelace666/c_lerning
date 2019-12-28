6a054b38ad8c743fbe43aef9991ad7d0e902e33a
nikita <nicrus2001@gmail.com>
2019-12-28 22:22:20 +0300

    3

--- test_1.c
+++ test_1.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
-{  int x,y;
+{
+    int x,y;
   printf("Введите x ");
   scanf("%d",&x);
-  printf(Результат );
+  printf("Результат");
    printf("%d",y=(3*(x*x*x*x*x*x))-(6*(x*x))-7);
      return 0;
 }
