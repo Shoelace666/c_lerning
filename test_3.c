486a3b1e08cb904cc2802c05675fbd72dd6ff0e1
nikita <nicrus2001@gmail.com>
2019-12-30 11:31:59 +0300

    test3

---
+++ test_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+	{
+int x;
+double s1,s2,s3;
+    printf("X=");
+     scanf("%d",&x);
+		 	printf("Площадь грани куба= ""%lf\n",s1=(x*x));
+			printf("Площадь поверхности куба=""%lf\n",s2=6*(x*x));
+			printf("Объем куба=""%lf",s3=x*x*x);
+
+
+
+	return 0;
+}
